6e2ceeb8277ddac6a762a4966d13596ca04de5d1
Kelvin Zhang <zhangxp1998@gmail.com>
2017-03-21 07:55:16 -0400

    setup the project

---
+++ src/MoveToFront.java
@@ -0,0 +1,98 @@
+import java.io.IOException;
+
+public class MoveToFront
+{
+	// apply move-to-front encoding, reading from standard input and writing to
+	// standard output
+
+	public static void encode() throws IOException
+	{
+		// Simulate a link list
+		int[] table = new int[256];
+		int head = 0;
+		for (int i = 0; i < table.length; i++)
+			table[i] = i + 1;
+
+		int c = 0;
+		while ((c = System.in.read()) != -1)
+		{
+			if (c == head)
+			{
+				System.out.write(0);
+			} else
+			{
+				int index = 1;
+				int cur = head;
+				while (table[cur] < table.length)
+				{
+					if (table[cur] == c)
+					{
+						System.out.write(index);
+						int tmp = table[cur];
+						// unlink table[cur] from link list
+						table[cur] = table[table[cur]];
+
+						// move that node to the front of the list
+						table[tmp] = head;
+						head = tmp;
+						break;
+					}
+					cur = table[cur];
+					index++;
+				}
+			}
+		}
+	}
+
+	// apply move-to-front decoding, reading from standard input and writing to
+	// standard output
+	public static void decode() throws IOException
+	{
+		// Simulate a link list, this is the table that stores the next pointers
+		int[] table = new int[256];
+		// head of the link list
+		int head = 0;
+
+		// initialize the next pointers
+		for (int i = 0; i < table.length; i++)
+			table[i] = i + 1;
+
+		int c = 0;
+		while ((c = System.in.read()) != -1)
+		{
+			int index = 0;
+			int cur = head;
+			int prev = -1;
+			while (index != c)
+			{
+				prev = cur;
+				cur = table[cur];
+				index++;
+			}
+			System.out.write(cur);
+
+			// Don't do anything if the char is already the head
+			if (prev == -1)
+				continue;
+
+			int tmp = table[prev];
+			// unlink table[prev] from link list
+			table[prev] = table[tmp];
+
+			// move that node to the front of the list
+			table[tmp] = head;
+			head = tmp;
+			assert head == cur;
+		}
+	}
+
+	// if args[0] is '-', apply move-to-front encoding
+	// if args[0] is '+', apply move-to-front decoding
+	public static void main(String[] args) throws IOException
+	{
+		if (args[0].equals("-"))
+			encode();
+		else if (args[0].equals("+"))
+			decode();
+	}
+}
---
+++ test/MoveToFrontTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class MoveToFrontTest
+{
+
+	public void hexdump(byte[] arr)
+	{
+		for (byte c : arr)
+			System.out.printf("%02X ", c);
+
+		System.out.println();
+	}
+
+	@Test
+	public void testEncode() throws IOException
+	{
+		InputStream stdin = System.in;
+		PrintStream stdout = System.out;
+
+		String str = "ABRACADABRA!";
+
+		InputStream is = new ByteArrayInputStream(str.getBytes(StandardCharsets.US_ASCII));
+		ByteArrayOutputStream os = new ByteArrayOutputStream(str.length());
+		System.setIn(is);
+		System.setOut(new PrintStream(os));
+
+		MoveToFront.encode();
+		System.setIn(stdin);
+		System.setOut(stdout);
+
+		byte[] solution = { 0x41, 0x42, 0x52, 0x02, 0x44, 0x01, 0x45, 0x01, 0x04, 0x04, 0x02, 0x26 };
+		
+		assertTrue("Different from solution arr!", Arrays.equals(solution, os.toByteArray()));
+	}
+	
+	@Test
+	public void testDecode() throws IOException
+	{
+		InputStream stdin = System.in;
+		PrintStream stdout = System.out;
+
+		String str = "ABRACADABRA!";
+		byte[] solution = { 0x41, 0x42, 0x52, 0x02, 0x44, 0x01, 0x45, 0x01, 0x04, 0x04, 0x02, 0x26 };
+
+		InputStream is = new ByteArrayInputStream(solution);
+		ByteArrayOutputStream os = new ByteArrayOutputStream(str.length());
+		System.setIn(is);
+		System.setOut(new PrintStream(os));
+
+		MoveToFront.encode();
+		System.setIn(stdin);
+		System.setOut(stdout);
+
+		System.out.println(new String(os.toByteArray()));
+	}
+}
