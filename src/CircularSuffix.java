bb6d945af9fdc93d61f3073a3bc5ba5dcb0ac919
Kelvin Zhang <zhanxp1998@gmail.com>
2017-03-21 18:59:40 -0400

    start LSD sort

--- src/CircularSuffix.java
+++ src/CircularSuffix.java
@@ -4,19 +4,19 @@ public class CircularSuffix
 	//Length of the string
 	private final int N;
 	//Actual string
-	private char[] data;
+	private byte[] data;
 	
 	//Number of characters the string shifted to the left
 	private final int j;
 
-	public CircularSuffix(char[] data, int offset)
+	public CircularSuffix(byte[] data, int offset)
 	{
 		this.N = data.length;
 		this.data = data;
 		this.j = offset;
 	}
 
-	public char digit(int index)
+	public byte digit(int index)
 	{
 		return data[(index + j) % N];
 	}
@@ -24,5 +24,10 @@ public class CircularSuffix
 	public int offset()
 	{
 		return j;
+	}
+	
+	public int length()
+	{
+		return N;
 	}
 }
--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -3,6 +3,7 @@ public class CircularSuffixArray
 	private String s;
 	private CircularSuffix[] sorted;
 	private int[] index;
+	private static final int R = 256;
 
 	public CircularSuffixArray(String s) // circular suffix array of s
 	{
@@ -11,25 +12,44 @@ public class CircularSuffixArray
 
 		this.s = s;
 
-		char[] data = s.toCharArray();
+		byte[] data = s.getBytes();
 
-		//Fill the Array with all possible circular suffixes.
+		// Fill the Array with all possible circular suffixes.
 		sorted = new CircularSuffix[s.length()];
 		for (int i = 0; i < sorted.length; i++)
 			sorted[i] = new CircularSuffix(data, i);
-		
-		//apply LSD sort
+
+		// apply LSD sort
 		sort(sorted);
-		
-		//process for index look up.
+
+		// process for index look up.
 		for (int i = 0; i < sorted.length; i++)
 			index[sorted[i].offset()] = i;
 	}
 
-	private static void sort(CircularSuffix[] arr)
+	private static void sort(CircularSuffix[] a)
 	{
-		CircularSuffix[] aux = new CircularSuffix[arr.length];
-		
+		final int L = a[0].length();
+		final int n = a.length;
+		CircularSuffix[] aux = new CircularSuffix[a.length];
+		int[] count = new int[R + 1];
+		for (int i = 0; i < L; i++)
+		{
+			for (int j = 0; j < a.length; j++)
+				count[a[j].digit(i) + 1]++;
+			
+			// compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+
+            // move data
+            for (int i = 0; i < n; i++)
+                aux[count[a[i].digit(d)]++] = a[i];
+
+            // copy back
+            for (int i = 0; i < n; i++)
+                a[i] = aux[i];
+		}
 	}
 
 	public int length() // length of s
