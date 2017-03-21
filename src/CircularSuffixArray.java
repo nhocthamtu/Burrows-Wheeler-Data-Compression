59e86458bb6be7a04258700c60cb2b9ff7de2350
Kelvin Zhang <zhanxp1998@gmail.com>
2017-03-21 19:28:45 -0400

    correct LSD sort copy back

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -22,6 +22,7 @@ public class CircularSuffixArray
 		// apply LSD sort
 		sort(sorted);
 
+		index = new int[sorted.length];
 		// process for index look up.
 		for (int i = 0; i < sorted.length; i++)
 			index[sorted[i].offset()] = i;
@@ -48,14 +49,14 @@ public class CircularSuffixArray
 			for (int i = 0; i < n; i++)
 				aux[count[a[i].digit(d)]++] = a[i];
 
-			// copy back
+			//Swap aux and a for next round
 			CircularSuffix[] tmp = a;
 			a = aux;
 			aux = tmp;
 		}
 		
 		for (int i = 0; i < n; i++)
-			a[i] = aux[i];
+			aux[i] = a[i];
 		
 	}
 
@@ -67,5 +68,13 @@ public class CircularSuffixArray
 	public int index(int i) // returns index of ith sorted suffix
 	{
 		return index[i];
+	}
+	
+	public void print()
+	{
+		for(CircularSuffix cs : sorted)
+		{
+			System.out.println(cs.toString());
+		}
 	}
 }
