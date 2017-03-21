68a62a1bb3de5f8ae7c61c179110dfc9d7d8b0c9
Kelvin Zhang <zhanxp1998@gmail.com>
2017-03-21 18:44:32 -0400

    add offset API in CircularSuffix, add comments

--- src/CircularSuffix.java
+++ src/CircularSuffix.java
@@ -1,19 +1,28 @@
 
 public class CircularSuffix
 {
+	//Length of the string
 	private final int N;
+	//Actual string
 	private char[] data;
-	int j;
 	
+	//Number of characters the string shifted to the left
+	private final int j;
+
 	public CircularSuffix(char[] data, int offset)
 	{
 		this.N = data.length;
 		this.data = data;
 		this.j = offset;
 	}
-	
+
 	public char digit(int index)
 	{
 		return data[N - index - j - 1];
+	}
+
+	public int offset()
+	{
+		return j;
 	}
 }
--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -1,16 +1,23 @@
 public class CircularSuffixArray
 {
 	private String s;
-	private char[][] sorted;
+	private CircularSuffix[] sorted;
 	private int[] index;
-	
+
 	public CircularSuffixArray(String s) // circular suffix array of s
 	{
-		if(s == null)
+		if (s == null)
 			throw new NullPointerException();
-		
-		
+
 		this.s = s;
+		
+		char[] data = s.toCharArray();
+
+		sorted = new CircularSuffix[s.length()];
+		for (int i = 0; i < sorted.length; i++)
+		{
+			sorted[i] = new CircularSuffix(data, i);
+		}
 	}
 
 	public int length() // length of s
